56f5c5bcff2b446d74dfe25f7980ab636332d250
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-29 09:23:23 +0500

    update

--- calculator.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem Calculate the relative path to the bin directory from the batch file's directory
-set "BIN_DIR=%~dp0bin"
-
-rem Run the TypeScript calculator using Node.js
-node "%BIN_DIR%\calculator.js" %*
